ec0f2df562d2fca584f7bf1a674f30dfabef0594
r.ron@gl-f.com <r.ron@gl-f.com>
2018-03-28 16:44:23 +0700

    add keyword

---
+++ src/main/java/com/khmersolution/moduler/web/api/KeywordController.java
@@ -0,0 +1,53 @@
+package com.khmersolution.moduler.web.api;
+
+import com.khmersolution.moduler.configure.Route;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+Create By: Ron Rith
+Create Date: 3/28/2018
+*/
+@RestController
+@RequestMapping(value = Route.API, produces = MediaType.APPLICATION_JSON_VALUE)
+public class KeywordController {
+    RestTemplate template = new RestTemplate();
+
+    @RequestMapping(value = "cokeywords", method = RequestMethod.GET)
+    public String getWeywordByTemplate(@RequestParam(value = "lastUpdate", required = false) String lastUpdate,
+                                        @RequestParam(value = "product", required = true) String product) {
+        HttpHeaders headers = new HttpHeaders();
+        Map<String, String> param = new HashMap<>();
+        param.put("lastUpdate", lastUpdate);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+        if (product != null && !product.equals("") && product.equals("HD")){
+            return template.exchange(Route.HD_BASE_URL + "/keyword/list_cokeywords", HttpMethod.POST, entity, String.class, param).getBody();
+        }else {
+            return template.toString();
+        }
+
+    }
+    @RequestMapping(value = "cokeywords/np", method = RequestMethod.GET)
+    public String getKeyWordByTemplate(@RequestParam(value = "lastUpdate", required = false) String lastUpdate) {
+        HttpHeaders headers = new HttpHeaders();
+        Map<String, String> param = new HashMap<>();
+        param.put("lastUpdate", lastUpdate);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+
+        return template.exchange(Route.HD_BASE_URL + "/keyword/list_cokeywords", HttpMethod.POST, entity, String.class, param).getBody();
+
+    }
+}
