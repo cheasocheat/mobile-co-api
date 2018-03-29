47dc2c2cfe914bd3e92f2a7e9a14c4b07fe9d0e9
r.ron@gl-f.com <r.ron@gl-f.com>
2018-03-29 11:05:07 +0700

    add quotation applicant controller

---
+++ src/main/java/com/khmersolution/moduler/web/api/QuotationApplicantController.java
@@ -0,0 +1,37 @@
+package com.khmersolution.moduler.web.api;
+
+import com.khmersolution.moduler.configure.Route;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+
+/*
+Create By: Ron Rith
+Create Date: 3/29/2018
+*/
+@RestController
+@RequestMapping(value = Route.API, produces = MediaType.APPLICATION_JSON_VALUE)
+public class QuotationApplicantController {
+    RestTemplate restTemplate = new RestTemplate();
+
+    @RequestMapping(value = "/quotationapplicant", method = RequestMethod.GET)
+    public String getQuotationApplicantByTemplate(@RequestParam(value = "lastUpdate", required = false) String lastUpdate,
+                                         @RequestParam(value = "product", required = true) String product) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        HttpEntity<String> entity = new HttpEntity<String>(lastUpdate, headers);
+        if (product != null && !product.equals("") && product.equals("HD")){
+            return restTemplate.exchange(Route.HD_BASE_URL + "/quotationapplicant/list_quotationapplicant", HttpMethod.POST, entity, String.class).getBody();
+        }else {
+            return restTemplate.toString();
+        }
+    }
+}
